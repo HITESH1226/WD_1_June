e70d5b2ae5b66dfadba030470fba82d1706b0147
HITESH1226 <hiteshsonavane@gmail.com>
2026-06-19 11:53:48 +0530

    js

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+    console.log("This is console");
+    alert("This is alert")
+    confirm("This is confirm")
+    document.writeln("This is New document")
+    document.write("This is Document")
+
+
+    // input
+
+    let a= prompt("Enter your Name:")
+    document.writeln("Your Name Is ",a)
---
+++ JS/string.js
@@ -0,0 +1,80 @@
+
+
+let a= "Hitesh"
+console.log(a);
+console.log(typeof(a));
+
+let b=a.charAt(3)
+console.log(b);
+
+let c=a.charCodeAt(3)
+console.log(c);
+
+let d=a.endsWith('h')
+console.log(d);
+
+let e=a.startsWith('h')
+console.log(e);
+
+let f=a.includes('z')
+console.log(f);
+
+let g=a.toLocaleUpperCase()
+console.log(g);
+
+let h=a.toLocaleLowerCase()
+console.log(h);
+
+let i=a.concat(" Sonavane")
+console.log(i);
+
+let j=i.replace("Hitesh","Varun")
+console.log(j);
+
+let k=a.repeat(10)
+console.log(k);
+
+
+// Number Method
+// 34 56.78
+
+let ab=34
+console.log(ab);
+console.log(typeof(ab));
+
+let xy=67.49865546
+console.log(xy);
+console.log(typeof(xy));
+
+let yz=xy.toFixed(2)
+console.log(yz);
+
+let uv=ab.toString
+console.log(uv);
+console.log(typeof(uv));
+
+// Boolean
+
+let m=true
+console.log(typeof(m));
+
+//null
+
+let n=null
+console.log(typeof(n));
+
+let p;
+console.log(p);
+
+
+
+
+
+
+
+
+
+
+
+
+
